9b9bf282d0ba90019e5ca320e5d83c598b60af46
root <abdelwahab.hamada@gmail.com>
2023-10-23 05:17:07 -0700

    AirBnB WEB Static

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines the HBnB console."""
+"""HBnB console"""
 import cmd
 import re
 from shlex import split
@@ -32,9 +32,8 @@ def parse(arg):
 
 
 class HBNBCommand(cmd.Cmd):
-    """Defines the HolbertonBnB command interpreter.
-    Attributes:
-        prompt (str): The command prompt.
+    """
+    HBNBCommand props
     """
 
     prompt = "(hbnb) "
